e8e0cd89dc8633ab96588af1c49cd6188e2458f3
shakyavartika <64145763+shakyavartika@users.noreply.github.com>
2022-04-30 18:24:07 +0530

    Update server.js

--- AllTalks/server.js
+++ AllTalks/server.js
@@ -4,7 +4,8 @@ const express = require('express');
 const app = express();
 /*Server*/
 const server = require('http').Server(app);
-
+/* importing socket.io */
+const io = require('socket.io')(server)
 /*importing uuid into this server.js */
 const {v4: uuidv4 } = require('uuid');
 app.set('view engine','ejs');
@@ -19,16 +20,45 @@ const io = require('socket.io')(server)
 /*importing uuid into this server.js */
 const {v4: uuidv4 } = require('uuid');
 app.set('view engine','ejs');
+/*importing peer.js */
+const { ExpressPeerServer } = require('peer');
 
-
-
+const peerServer = ExpressPeerServer(server, {
+    debug:true
+});
 
 app.use(express.static('public'));
 /* the main route is the roomid so when we go the root it will generate the room id and pass it as a parameter */
 app.get('/', (req, res) => {
     res.redirect(`/${uuidv4()}`);
   })
+/*hey what url are you going to use? */
+app.use('/peerjs',peerServer);
 
+app.get('/:room',(req,res)=> {
+    res.render('room', { roomId:req.params.room});
+})
+/*building connection */
+io.on('connection', socket => {
+    socket.on('join-room' , (roomId, userId) => {
+        socket.join(roomId);
+        socket.to(roomId).emit('user-connected',userId);
+        socket.on('message',message => {
+            io.to(roomId).emit('createMessage',message);
+        });
+        socket.on("disconnect", () => { // When a user disconnects or leaves
+            socket.to(roomId).emit("user-disconnected", userId);
+        });
+        
+        socket.on('subsitle',result => {
+            io.to(roomId).emit('SubTtile' ,result);
+        });
+        socket.on("disconnect", () => { // When a user disconnects or leaves
+            socket.to(roomId).emit("user-disconnected", id);
+        });
+        
+    });
+});
 
 /*for localhost */
 
